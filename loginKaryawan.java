f48edc176d3bcf07f266025797178a16faeda139
anyacallissta <vtjhs18@gmail.com>
2023-10-18 21:41:45 +0700

    mengubah fitur login

--- loginKaryawan.java
+++ loginKaryawan.java
@@ -6,21 +6,50 @@ public class loginKaryawan {
 
     public static void main(String[] args) {
         Scanner input = new Scanner(System.in);
-        String username, password;
-        
-        while (true) {
-            System.out.print("Masukkan Username Anda : ");
-            username = input.nextLine();
-            System.out.print("Masukkan Password Anda : ");
-            password = input.nextLine();
-            
-            if (username.equals("user123") && password.equals("pass123")) {
-                System.out.println("Anda Berhasil Login");
-                break;
-            } else {
-                System.out.println("Username dan Password Anda Salah!");
-            }
-        }
+        String username, password, nik, user;
 
-    }
+        System.out.println("===WELCOME TO EKSPEDISI===");
+        System.out.print("LOGIN SEBAGAI : ");
+        user = input.nextLine();
+
+        if (user.equalsIgnoreCase("karyawan")) {
+
+            while (true) {
+                System.out.print("Masukkan Username Anda                    : ");
+                username = input.nextLine();
+                System.out.print("Masukkan Password Anda                    : ");
+                password = input.nextLine();
+                System.out.print("Masukkan Nomor Identitas Karyawan Anda    : ");
+                nik = input.nextLine();
+
+                if (username.equals("kry123") && password.equals("kry123") && nik.equals("20230123")) {
+                    System.out.println("LOGIN SUCCESSFUL");
+                    break;
+                } else {
+                    System.out.println("LOGIN FAILED! (masukkan kembali dengan data yang benar)");
+                }
+           
+            }
+        }
+        else if (user.equalsIgnoreCase("pelanggan")) {
+
+            while (true) {
+                System.out.print("Masukkan Username Anda                    : ");
+                username = input.nextLine();
+                System.out.print("Masukkan Password Anda                    : ");
+                password = input.nextLine();
+
+                if (username.equals("cus123") && password.equals("cus123")) {
+                    System.out.println("LOGIN SUCCESSFUL");
+                    break;
+                } else {
+                    System.out.println("LOGIN FAILED! (masukkan kembali dengan data yang benar)");
+                }
+           
+            }
+        }
+        else
+            System.out.print("===OUT OF SYSTEM===");
+        
+    }
 }
