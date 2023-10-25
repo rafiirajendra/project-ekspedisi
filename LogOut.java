7c27b1dce6943d151263bf762eb4e92620c0a01b
anyacallissta <vtjhs18@gmail.com>
2023-10-25 08:38:34 +0700

    first commit

--- LogOut.java
+++ LogOut.java
@@ -16,7 +16,7 @@ public class LogOut {
 
             switch (pilihan) {
                 case 1 :
-                ///login
+                //login
                 break;
 
                 case 2 :
