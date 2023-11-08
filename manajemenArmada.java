8759e73cb563f725b43d46624782da3ee886b763
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-11-08 11:21:39 +0700

    penambahan nested loop

--- manajemenArmada.java
+++ manajemenArmada.java
@@ -1,19 +1,24 @@
 import java.util.Scanner;
 
-/**
- * manajemenArmada
- */
 public class manajemenArmada {
 
     public static void main(String[] args) {
-        Scanner inputlo = new Scanner(System.in);
-        System.out.print("Masukkan total berat barang (dalam kilogram): ");
-        float beratBarang = inputlo.nextFloat();
-        
-        if (beratBarang >= 120) {
-            System.out.println("Barang dikirim menggunakan mobil");
-        } else {
-            System.out.println("Barang dikirim menggunakan motor");
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukkan jumlah barang: ");
+        int jumlahBarang = input.nextInt();
+
+        for (int i = 1; i <= jumlahBarang; i++) {
+            System.out.print("Masukkan berat barang ke-" + i + " (dalam kilogram): ");
+            float beratBarang = input.nextFloat();
+
+            for (int j = 1; j <= 1; j++) {
+                if (beratBarang >= 120) {
+                    System.out.println("Barang ke-" + i + " dikirim menggunakan mobil");
+                } else {
+                    System.out.println("Barang ke-" + i + " dikirim menggunakan motor");
+                }
+            }
         }
     }
 }
