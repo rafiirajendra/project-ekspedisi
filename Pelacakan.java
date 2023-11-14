b5041b9bdd435af41ef19ed15a4e24d89eb8198f
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-11-14 18:59:25 +0700

    perubahan nama variabel

--- Pelacakan.java
+++ Pelacakan.java
@@ -7,11 +7,11 @@ public class Pelacakan {
         System.out.print("Masukkan nommor resi: ");
         int resi = sc.nextInt();
 
-        String[] strings = {"Proses pengiriman", "Paket sudah sampai"};
+        String[] status = {"Proses pengiriman", "Paket sudah sampai"};
         Random random = new Random();
-        int randomIndex = random.nextInt(strings.length); 
-        String randomString = strings[randomIndex];
+        int acak = random.nextInt(status.length); 
+        String statusPaket = status[acak];
     
-        System.out.println("Status Paket: " + randomString);
+        System.out.println("Status Paket: " + statusPaket);
     }
 }
