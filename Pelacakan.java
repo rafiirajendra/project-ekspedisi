cee8373a766a3fef3d2330bb3c1d46cf5c11d983
Muhammad Rafi Rajendra <rafi53417@gmail.com>
2023-11-22 10:02:35 +0700

    menambahkan fungsi

--- Pelacakan.java
+++ Pelacakan.java
@@ -2,16 +2,19 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Pelacakan {
-    public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan nommor resi: ");
-        int resi = sc.nextInt();
-
+    public static String lacakStatusPaket(int resi) {
         String[] status = {"Proses pengiriman", "Paket sudah sampai"};
         Random random = new Random();
         int acak = random.nextInt(status.length); 
-        String statusPaket = status[acak];
+        return status[acak];
+    }
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nomor resi: ");
+        int resi = sc.nextInt();
     
+        String statusPaket = lacakStatusPaket(resi);
         System.out.println("Status Paket: " + statusPaket);
     }
 }
