7d40cd5427daba9cd87acdad50cf10f4ba7db466
anyacallissta <vtjhs18@gmail.com>
2023-11-25 09:47:01 +0700

    add perulangan untuk melacak paket

--- Pelacakan.java
+++ Pelacakan.java
@@ -2,6 +2,14 @@ import java.util.Random;
 import java.util.Scanner;
 
 public class Pelacakan {
+
+    public static void inputResi() {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Masukkan nomor resi: ");
+        int resi = sc.nextInt();
+        String statusPaket = lacakStatusPaket(resi);
+        System.out.println("Status Paket: " + statusPaket);
+    }
     public static String lacakStatusPaket(int resi) {
         String[] status = {"Proses pengiriman", "Paket sudah sampai"};
         Random random = new Random();
@@ -10,11 +18,20 @@ public class Pelacakan {
     }
 
     public static void main(String[] args) {
+        System.out.println("===================");
+        System.out.println("= PELACAKAN PAKET =");
+        System.out.println("===================");
         Scanner sc = new Scanner(System.in);
-        System.out.print("Masukkan nomor resi: ");
-        int resi = sc.nextInt();
-    
-        String statusPaket = lacakStatusPaket(resi);
-        System.out.println("Status Paket: " + statusPaket);
+        inputResi();
+
+        while (true) {
+        System.out.print("Ingin melacak paket yang lain? (y/n): ");
+        String pilih = sc.nextLine();
+            if (pilih.equalsIgnoreCase("y")) {
+                inputResi();
+            } else {
+                System.exit(0);
+            }
+        }
     }
 }
