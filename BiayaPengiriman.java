b8bd1e17fb87fd5b8179af773ace216a2e5a7c44
kibarmustofa <kibarmustofa115@gmail.com>
2023-11-26 11:31:29 +0700

    mengubah tampilan output

--- BiayaPengiriman.java
+++ BiayaPengiriman.java
@@ -1,43 +1,54 @@
 import java.util.Scanner;
 
-public class biayaPengiriman {
+public class BiayaPengiriman {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         int biayaKm = 4000, biayaBrt = 5000;
         float jarakPengiriman, beratBarang, biayaPengiriman;
 
-        System.out.println("Masukkan jarak (dalam km): ");
+        System.out.println("==============================");
+        System.out.println("=   Program Biaya Pengiriman  =");
+        System.out.println("==============================");
+
+        System.out.print("Masukkan jarak pengiriman (dalam km): ");
         jarakPengiriman = sc.nextFloat();
-
-        System.out.println("Masukkan berat barang (dalam kg): ");
+        System.out.print("Masukkan berat barang (dalam kg): ");
         beratBarang = sc.nextFloat();
-
-        System.out.println("Pilih jenis layanan (reguler/ekspres): ");
+        System.out.print("Pilih jenis layanan (reguler/ekspres): ");
         String jenisLayanan = sc.next();
+
+        System.out.println("\nMenghitung biaya pengiriman...");
 
         biayaPengiriman = jarakPengiriman * biayaKm + beratBarang * biayaBrt;
 
         if (jarakPengiriman <= 10) {
             if (beratBarang <= 5) {
-                biayaPengiriman *= 0.8;  // ini merupakan pajak tarif pengiriman jarak kurang dari 10 km 
+                biayaPengiriman *= 0.8;
             }
         } else if (jarakPengiriman > 10 && jarakPengiriman <= 50) {
             if (beratBarang > 5) {
-                biayaPengiriman *= 1.2;  // ini pajak tarif pengiriman untuk jarak untuk jarak lebih dari 10 km hingga kurang lebih dari 50
+                biayaPengiriman *= 1.2;
             }
         } else {
-            biayaPengiriman *= 1.5;  // ini pajak tarif tambahan untuk jarang yang lebih jauh 
+            biayaPengiriman *= 1.5;
         }
+
         float biayaReguler = biayaPengiriman;
+        float biayaEkspres = biayaPengiriman *= 1.5;
 
-        float biayaEkspres = biayaPengiriman *= 1.5; //pajak 
+        System.out.println("=== Hasil Perhitungan ===");
+        System.out.println("Jarak Pengiriman   : " + jarakPengiriman + " km");
+        System.out.println("Berat Barang       : " + beratBarang + " kg");
+        System.out.println("Jenis Layanan      : " + jenisLayanan);
 
-        if (jenisLayanan.equals("reguler")) {
-            System.out.println("Biaya pengiriman reguler adalah: " + biayaReguler);
-    
+        if (jenisLayanan.equalsIgnoreCase("reguler")) {
+            System.out.println("\nBiaya Pengiriman (Reguler) : Rp" + biayaReguler);
+        } else if (jenisLayanan.equalsIgnoreCase("ekspres")) {
+            System.out.println("Biaya Pengiriman (Ekspres) : Rp" + biayaEkspres);
         } else {
-            System.out.println("Biaya pengiriman ekspres adalah: " + biayaEkspres);
-       
+            System.out.println("Jenis layanan tidak valid.");
         }
+
+        System.out.println("\nTerima kasih telah menggunakan program biaya pengiriman.");
     }
 }
