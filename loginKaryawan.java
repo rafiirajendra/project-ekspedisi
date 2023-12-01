fbd00e8547478049a129b1b59929b617db5f885f
anyacallissta <vtjhs18@gmail.com>
2023-12-01 22:02:14 +0700

    add fungsi log out + exit

--- loginKaryawan.java
+++ loginKaryawan.java
@@ -1,81 +1,72 @@
 import java.util.Scanner;
-/**
- * loginKaryawan
- */
+
 public class loginKaryawan {
 
-    /**
-     * @param args
-     */
+    public static void logout() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Apakah Anda ingin Logout? (Y/y)");
+        char jawab = sc.nextLine().charAt(0);
+        if (Character.toLowerCase(jawab) == 'y') {
+            System.out.println("LOGOUT SUCCESSFUL");
+        } else {
+            System.out.println("Kembali ke menu.");
+        }
+    }
+
+    public static void exit() {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Apakah Anda yakin ingin keluar dari Ekspedisi? (Y/y)");
+        char jawab = sc.nextLine().charAt(0);
+        if (Character.toLowerCase(jawab) == 'y') {
+            System.out.println("Terimakasih telah menggunakan Ekspedisi kami.");
+            System.exit(0);
+        } else {
+            System.out.println("Kembali ke menu.");
+        }
+    }
+
     public static void main(String[] args) {
-        Scanner input = new Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
         String username, password, nik, user;
-        int pilihan;
+        
+        System.out.println("==========================");
+        System.out.println("=  WELCOME TO EKSPEDISI  =");
+        System.out.println("==========================");
+        System.out.print("LOGIN SEBAGAI (karyawan/pelanggan): ");
+        user = sc.nextLine();
 
-        while (true) {
-            System.out.println("1. Login");
-            System.out.println("2. Logout");
-            System.out.println("3. Exit");
-            System.out.print("Pilih operasi (1/2/3) : ");
-            pilihan = input.nextInt();
-            input.nextLine();
-
-            switch (pilihan) {
-                case 1 :
-                
-                System.out.println("==========================");
-                System.out.println("=  WELCOME TO EKSPEDISI  =");
-                System.out.println("==========================");
-                System.out.print("LOGIN SEBAGAI : ");
-                user = input.nextLine();
-
-                if (user.equalsIgnoreCase("karyawan")) {
-                    while (true) {
-                        System.out.print("Masukkan Username Anda                    : ");
-                        username = input.nextLine();
-                        System.out.print("Masukkan Password Anda                    : ");
-                        password = input.nextLine();
-                        System.out.print("Masukkan Nomor Identitas Karyawan Anda    : ");
-                        nik = input.nextLine();
-                        if (username.equals("kary123") && password.equals("kary123") && nik.equals("20230123")) {
-                            System.out.println("LOGIN SUCCESSFUL");
-                            break;
-                        } else {
-                            System.out.println("LOGIN FAILED! Masukkan kembali dengan data yang benar.");
-                        }
-                    }
+        if (user.equalsIgnoreCase("karyawan")) {
+            while (true) {
+                System.out.print("Masukkan Username Anda                    : ");
+                username = sc.nextLine();
+                System.out.print("Masukkan Password Anda                    : ");
+                password = sc.nextLine();
+                System.out.print("Masukkan Nomor Identitas Karyawan Anda    : ");
+                nik = sc.nextLine();
+                if (username.equals("kary123") && password.equals("kary123") && nik.equals("20230123")) {
+                    System.out.println("LOGIN SUCCESSFUL");
+                    break;
+                } else {
+                    System.out.println("LOGIN FAILED! Masukkan kembali dengan data yang benar.");
                 }
-                else if (user.equalsIgnoreCase("pelanggan")) {
-                    while (true) {
-                        System.out.print("Masukkan Username Anda                    : ");
-                        username = input.nextLine();
-                        System.out.print("Masukkan Password Anda                    : ");
-                        password = input.nextLine();
-                        if (username.equals("cus123") && password.equals("cus123")) {
-                            System.out.println("LOGIN SUCCESSFUL");
-                            break;
-                        } else {
-                            System.out.println("LOGIN FAILED! Masukkan kembali dengan data yang benar.");
-                        }
-                    }
-                }
-                break;
-
-                case 2 :
-                System.out.println("LOGOUT SUCCESS");
-                break;
-
-                case 3 :
-                System.out.print("Apakah Anda yakin ingin keluar dari Ekspedisi? (Y/y) ");
-                char jawab = input.nextLine().charAt(0);
-                    if (Character.toLowerCase(jawab) == 'y') {
-                        System.out.println("Terimakasih telah menggunakan Ekspedisi kami.");
-                        System.exit(0);
-                    } else {
-                        System.out.println("Kembali ke menu.");
-                    }
-                break;
             }
-        } 
+        }
+        else if (user.equalsIgnoreCase("pelanggan")) {
+            while (true) {
+                System.out.print("Masukkan Username Anda : ");
+                username = sc.nextLine();
+                System.out.print("Masukkan Password Anda : ");
+                password = sc.nextLine();
+                if (username.equals("cus123") && password.equals("cus123")) {
+                    System.out.println("LOGIN SUCCESSFUL");
+                    break;
+                } else {
+                    System.out.println("LOGIN FAILED! Masukkan kembali dengan data yang benar.");
+                }
+            }
+        }
+        else {
+            System.out.println("Pengguna yang dimasukkan tidak valid. Masukkan 'karyawan' atau 'pelanggan'.");
+        }
     }
 }
